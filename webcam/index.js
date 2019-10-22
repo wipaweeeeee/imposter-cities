06baa1ab8c09d369603e4f90d8e15bc266541950
Eva <renatusair@gmail.com>
2019-10-22 15:12:25 -0400

    Adding webcam streaming

---
+++ webcam/index.js
@@ -0,0 +1,20 @@
+const cv = require('opencv4nodejs');
+const express = require('express');
+const path = require('path');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const wCap = new cv.VideoCapture(0);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+setInterval(() => {
+    const frame = wCap.read();
+    const image = cv.imencode('.jpg', frame).toString('base64');
+    io.volatile.emit('image', image);
+}, 1000 / 16)
+
+server.listen(3002);
