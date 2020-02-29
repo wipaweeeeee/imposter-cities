ccaa848bd36546ba24336a0d166deb30913e5433
Eva <renatusair@gmail.com>
2020-02-28 19:07:03 -0500

    Merging with camera

--- webcam/index.js
+++ webcam/index.js
@@ -16,30 +16,6 @@ const wCap = DEVELOPMENT_MODE? new cv.VideoCapture(0) : new cv.VideoCapture(sett
 const bgSubtractor = new cv.BackgroundSubtractorMOG2();
 let numBlob;
 
-grabFrames('./green_trim.MOV', 50, (frame) => {
-  const foreGroundMask = bgSubtractor.apply(frame);
-
-  const iterations = 1;
-  const dilated = foreGroundMask.dilate(
-    cv.getStructuringElement(cv.MORPH_ELLIPSE, new cv.Size(4, 4)),
-    new cv.Point(-1, -1),
-    iterations
-  );
-  const blurred = dilated.blur(new cv.Size(10, 10));
-  const thresholded = blurred.threshold(200, 255, cv.THRESH_BINARY);
-
-  const minPxSize = 6000;
-  // drawRectAroundBlobs(thresholded, frame, minPxSize);
-  numBlob = drawRectAroundBlobs(thresholded, frame, minPxSize);
-  // console.log(numBlob);
-
-  // cv.imshow('foreGroundMask', foreGroundMask);
-  // cv.imshow('thresholded', thresholded);
-  // cv.imshow('frame', frame);
-
-})
-
-
 io.on('connection', function(socket){
 	console.log('connection found')
 });
@@ -53,8 +29,8 @@ setInterval(() => {
     // Optimization
     let frameOpt = frame.resizeToMax(500);
     frameOpt = frameOpt.convertTo(cv.CV_64FC3);
+    let blobs_count = grabFramesHelper(frameOpt);
     const image = cv.imencode('.jpg', frameOpt).toString('base64');
-
     DEVELOPMENT_MODE && console.log(byteCount(image))
     io.volatile.emit('data', {image: image, numBlob: numBlob});
 }, 1000 / 16)
@@ -70,3 +46,24 @@ app.get('/', (req, res) => {
 
 server.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
+function grabFramesHelper(frame){
+  const foreGroundMask = bgSubtractor.apply(frame);
+
+  const iterations = 1;
+  const dilated = foreGroundMask.dilate(
+    cv.getStructuringElement(cv.MORPH_ELLIPSE, new cv.Size(4, 4)),
+    new cv.Point(-1, -1),
+    iterations
+  );
+  const blurred = dilated.blur(new cv.Size(10, 10));
+  const thresholded = blurred.threshold(200, 255, cv.THRESH_BINARY);
+
+  const minPxSize = 6000;
+  numBlob = drawRectAroundBlobs(thresholded, frame, minPxSize);
+
+  // cv.imshow('foreGroundMask', foreGroundMask);
+  // cv.imshow('thresholded', thresholded);
+  // cv.imshow('frame', frame);
+
+  return numBlob;
+}
